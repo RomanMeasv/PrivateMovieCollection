1f462f6156627d2e000228418bbd862dacf1a4d1
Ladam0203 <adam7605@easv365.dk>
2022-01-07 13:47:30 +0100

    Class setup (Movie, Category)
    properties, getters, setters, constructors

--- src/pmcollection/be/Movie.java
+++ src/pmcollection/be/Movie.java
@@ -1,4 +1,55 @@
 package pmcollection.be;
 
+import java.time.LocalDate;
+
 public class Movie {
+    private int id;
+    private String name;
+    private int rating;
+    private String filelink;
+    private LocalDate lastview;
+
+    public Movie(int id, String name, int rating, String filelink, LocalDate lastview) {
+        this.id = id;
+        this.name = name;
+        this.rating = rating;
+        this.filelink = filelink;
+        this.lastview = lastview;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getFilelink() {
+        return filelink;
+    }
+
+    public void setFilelink(String filelink) {
+        this.filelink = filelink;
+    }
+
+    public LocalDate getLastview() {
+        return lastview;
+    }
+
+    public void setLastview(LocalDate lastview) {
+        this.lastview = lastview;
+    }
 }
