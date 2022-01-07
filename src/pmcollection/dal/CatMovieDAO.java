c2ced8513c1d4362a6ab9c5fc5cae3d2f13a959d
Ladam0203 <adam7605@easv365.dk>
2022-01-07 14:12:56 +0100

    Filled CatMovieDAO and I

--- src/pmcollection/dal/CatMovieDAO.java
+++ src/pmcollection/dal/CatMovieDAO.java
@@ -1,4 +1,28 @@
 package pmcollection.dal;
 
-public class CatMovieDAO {
+import pmcollection.be.Category;
+
+import java.util.List;
+
+public class CatMovieDAO implements ICatMovieDA{
+
+    @Override
+    public int create() {
+        return 0;
+    }
+
+    @Override
+    public List<Category> read() {
+        return null;
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
 }
--- src/pmcollection/dal/ICatMovieDA.java
+++ src/pmcollection/dal/ICatMovieDA.java
@@ -1,4 +1,13 @@
 package pmcollection.dal;
 
+import pmcollection.be.Category;
+import pmcollection.be.Movie;
+
+import java.util.List;
+
 public interface ICatMovieDA {
+    int create();
+    List<Category> read();
+    void update();
+    void delete();
 }
