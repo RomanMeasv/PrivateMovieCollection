b0cf743d0843439d09941c990bebcd151b823f28
Ladam0203 <adam7605@easv365.dk>
2022-01-10 12:51:07 +0100

    Get all movies implemented in logic

--- src/pmcollection/bll/MovieLogic.java
+++ src/pmcollection/bll/MovieLogic.java
@@ -7,6 +7,8 @@ import pmcollection.dal.interfaces.IMovieDA;
 import pmcollection.dal.dao.MovieDAO;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MovieLogic {
@@ -19,7 +21,11 @@ public class MovieLogic {
     }
 
     public List<Movie> getAllMovies() throws Exception {
-        return movieDAO.getAllMovies();
+        List<Movie> allMovies = movieDAO.getAllMovies();
+        for (Movie movie : allMovies) {
+            movie.setCategories(catMovieDAO.getCategoriesOfMovieById(movie.getId()));
+        }
+        return allMovies;
     }
 
     public Movie getMovie(int id) throws Exception {
