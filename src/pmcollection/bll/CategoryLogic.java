b15c2dfec1064e95d87c309b27bf2586d565fc86
Ladam0203 <adam7605@easv365.dk>
2022-01-12 13:04:54 +0100

    CategoryLogic exceptions gracefully handled

--- src/pmcollection/bll/CategoryLogic.java
+++ src/pmcollection/bll/CategoryLogic.java
@@ -1,6 +1,8 @@
 package pmcollection.bll;
 
 import pmcollection.be.Category;
+import pmcollection.bll.exceptions.CatMovieException;
+import pmcollection.bll.exceptions.CategoryException;
 import pmcollection.dal.dao.CatMovieDAO;
 import pmcollection.dal.dao.CategoryDAO;
 import pmcollection.dal.interfaces.ICatMovieDA;
@@ -18,20 +20,30 @@ public class CategoryLogic {
         catMovieDAO = new CatMovieDAO();
     }
 
-    public List<Category> getAllCategories() throws Exception {
-        return this.categoryDAO.getAllCategories();
+    public List<Category> getAllCategories() throws CategoryException {
+        try
+        { return this.categoryDAO.getAllCategories(); }
+            catch (Exception e) { throw new CategoryException("Could not access movies from database!", e); }
     }
 
-    public Category addCategory(Category category) throws Exception {
-        return this.categoryDAO.createCategory(category);
+    public Category addCategory(Category category) throws CategoryException {
+        try
+        { return this.categoryDAO.createCategory(category); }
+            catch (Exception e) { throw new CategoryException("Could not create category \"" + category.getName() + "\" in the database!", e); }
     }
 
-    public void update(Category selected) throws Exception {
-        this.categoryDAO.updateCategory(selected);
+    public void update(Category category) throws CategoryException {
+        try
+        { this.categoryDAO.updateCategory(category); }
+            catch (Exception e) { throw new CategoryException("Could not update category \"" + category.getName() + "\" in the database!", e); }
     }
 
-    public void delete(Category selected) throws Exception {
-        catMovieDAO.unlinkCategoryFromMovies(selected);
-        this.categoryDAO.deleteCategory(selected);
+    public void delete(Category category) throws CatMovieException, CategoryException {
+        try
+        { catMovieDAO.unlinkCategoryFromMovies(category); }
+            catch (Exception e) { throw new CatMovieException("Could not unlink category \"" + category.getName() + "\" from movie it was assigned to in the database!", e); }
+        try
+        { this.categoryDAO.deleteCategory(category); }
+            catch (Exception e) { throw new CategoryException("Category \"" + category.getName() + "\" was unlinked from the movies it was assigned to, but the category itself could not be deleted from the database!", e); }
     }
 }
